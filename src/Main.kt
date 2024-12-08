9088e0a23dd4cf606a9521742bc6cc5ae39c304a
XYZboom <xyzboom@qq.com>
2024-12-08 17:39:50 +0800

    feat(func): add a function that minus two numbers

--- src/Main.kt
+++ src/Main.kt
@@ -1,8 +1,13 @@
 fun main() {
     println("Hello World!")
-    add(1,2)
+    add(1, 2)
+    println(minus(1, 2))
 }
 
 fun add(a: Int, b: Int): Int {
     return a + b
+}
+
+fun minus(a: Int, b: Int): Int {
+    return a - b
 }
