ad1a1a20383e9f992ccf5974d58b68ebc97972f6
vvvvvictoriaa <2197972028@qq.com>
2024-12-08 20:28:40 +0800

    feat(func): add a function that add three numbers

--- src/Main.kt
+++ src/Main.kt
@@ -1,8 +1,13 @@
 fun main() {
     println("Hello World!")
     add(1,2)
+    add3number(1,2,3)
 }
 
 fun add(a: Int, b: Int): Int {
     return a + b
+}
+
+fun add3number(a: Int, b: Int, c: Int): Int {
+    return a + b + c
 }
