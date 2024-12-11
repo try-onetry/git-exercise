2a19971bfed6d06b3fead50e5d602c525f48868f
vvvvvictoriaa <2197972028@qq.com>
2024-12-11 14:52:12 +0800

    Exercise1

---
+++ src/Exercise1.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Exercise1 {
+    public static void main(String[] args) {
+        List<Integer> number = new ArrayList<>();
+        number.add(0);
+        number.add(1);
+        number.add(2);
+        number.add(3);
+        printAll(number);
+    }
+    public static void printAll(List<Integer> list) {
+
+        System.out.println(list);
+
+        for(int i = 0; i < list.size(); i++){
+            System.out.println(list.get(i));
+        }
+
+        for (Integer integer : list) {
+            System.out.println(integer);
+        }
+    }
+
+
+}
