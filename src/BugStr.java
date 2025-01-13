d2c2f80d52799c688e8ff6501571dc1b4204eeef
vvvvvictoriaa <2197972028@qq.com>
2025-01-13 20:46:26 +0800

    update code formatting

--- src/BugStr.java
+++ src/BugStr.java
@@ -2,9 +2,11 @@ public class BugStr {
     public String a = null;
 
     public String str;
+
     public String getStr() {
         return str;
     }
+
     public void setStr(String str) {
         this.str = str;
     }
